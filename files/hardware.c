1b44e96b8d6b12de7e3f5be5bea074b6235e11d4
alesiuk <alesiuk@itba.edu.ar>
2020-06-08 23:43:56 -0300

    se puso el hardware porque otra cosa no la ponemos ni en pedo

--- files/hardware.c
+++ files/hardware.c
@@ -4,4 +4,62 @@ Este archivo contiene todas las funciones que se necesitan para dar salida a los
 Creado por el grupo 5
 */
 
+#include <stdio.h>
+#include "hardware.h"
 
+
+
+void hard (void){
+
+FILE * handle_driection = NULL;
+FILE * handle = NULL;
+char led[8]={17;4;18;23;24;25;22;27}
+int Written;
+int value;
+
+for (unsigned char i=0; i<8; i++){
+
+	if((handle_direction = PIN_SDIRECT(i)) == NULL){		//si ocurrio algun error en la direccion del pin devuelvo un 1 
+		printf("cannot open DIRECTION File\n");
+		exit(1);
+	}
+ 	else {
+		printf("DIRECTION File for PIN OPENED succefully\n");	//si no ocurrio ningun error el pin fue abierto satisfactoriamente 
+	}
+	if ((Written=fputs(“out”,handle_direction))==-1){
+		printf("Cannot open DIRECTION pin. Try again later.\n");
+		exit(1);
+	}
+	else{
+		printf("DIRECTION File for PIN opened succesfully\n");
+	}
+
+	fclose(handle_direction);
+
+	if ((handle = SPIN_VALUE(i))) == NULL){
+		printf("Cannot open device. Try again later.\n");
+		exit(1);
+	}
+	else{
+		printf("Device successfully opened\n");
+	}	
+
+ 	if(VALUE(i) == 1)
+		value = 1;
+ 	else 
+		value = 0;
+
+ 	if(PIN_VALUE(value))==-1) {
+		printf("Clr_Pin: Cannot write to file. Try again later.\n");
+		exit(1);
+	}
+
+	else
+		printf("Write successfully done.\n",);
+	}
+	
+	fclose(handle);
+	
+
+
+}
--- files/hardware.h
+++ files/hardware.h
@@ -3,3 +3,18 @@ hardware.h (Header)
 Este archivo contiene todas las funciones que se necesitan para dar salida a los datos utilizando los puertos de la RaspBerry Pi
 Creado por el grupo 5
 */
+
+#ifndef HARDWARE_H
+#define HARDWARE_H
+/*DEFINICION DE MACROS*/
+#define PIN_SDIRECT(x) fopen(“/sys/class/gpio/gpio##x/direction”,"w")
+#define PIN_SVALUE(x) fopen(“/sys/class/gpio/gpio##x/value”,"w")
+#define PIN_DIRECT(x) fputc("x",handle_direction)
+#define PIN_VALUE(x) fputc('x',handle)
+#define VALUE(x) p->portA_bit.b##x
+
+/*DEFINCION DE FUNCIONES*/
+
+void hard (void);
+
+#endif 
